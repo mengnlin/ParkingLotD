3a43f5d59000ad06f33d25c0b4da3ebcd6336e68
Yiou Chen <chen.yiou1992@gmail.com>
2019-10-03 11:49:50 -0700

    Update Rate.js

--- Rate.js
+++ Rate.js
@@ -1,4 +1,8 @@
 class Rate {
+  // Better create subclass that overrides the calculateRate function for hourly, daily and premium.
+  // They might have different logic other than the unitMinutes.
+  // For example, Daily might get the upper bound of days, so 1.5 days will be charged for 2 days
+  // Having subclass allows future modification to the logic for each Rate.
   constructor(unitPrice, unitMinutes) {
     this.unitPrice = unitPrice;
     this.unitMinutes = unitMinutes;
